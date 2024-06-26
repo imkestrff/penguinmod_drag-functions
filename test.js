fedf2d79717414c5a0e3fcf6af6ece9d64a97201
kopfklein füchsklein <156494247+imkestrff@users.noreply.github.com>
2024-06-25 19:04:23 -0700

    Update test.js
    functions properly now

--- test.js
+++ test.js
@@ -8,6 +8,7 @@
 				color1: "#42FF88",
 				color2: "#21BA63",
 				color3: "#166342",
+				//define blocks below
 				blocks: [
 					{
 					//test log block
@@ -23,15 +24,16 @@
 					disableMonitor: false,
 					allowDropAnywhere: true
 					}
-				],
+				]
 			};//end return
 		}//end get info  
+//call block definitions below
+		testLogToConsole(){
+			console.log('log!');
+		};
+		testReporter(){
+			return 'report!';
+		};
 	}//end class extension
-	testLogToConsole(){
-		console.log('log!')
-	};
-	testReporter(){
-		return 'report!'
-	};
 	Scratch.extensions.register(new Extension());
 })(Scratch);
